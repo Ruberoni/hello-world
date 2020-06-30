06be5a8b00ee9f32055971ae82e6afbd0bd203f7
Ruben <ruben.pardes25@gmail.com>
2020-06-30 15:40:38 -0400

    Remade the py doc

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,6 @@
-print("Hola Mundo", "SI")
-casa = "Nueva variable"
+casa = "Cocina Baño-1 Baño-2 Sala Cuarto-principal Cuarto-Secundario-1 Cuarto-Secundario-2 Patio Garaje"
+
+casa = casa.split()
+
+for cuarto in casa:
+    print(cuarto)
